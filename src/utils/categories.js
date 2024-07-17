9e38a01319a5b1d67205ba0c8cd0ec24f8b4f34c
Shohruz <shokhruzisroilov@gmail.com>
2024-07-17 17:40:07 +0500

    finished

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -1,7 +1,7 @@
 export const categories = [
-	{ id: 'dish', name: 'Блюдо' },
+	{ id: 'dish', name: 'Блюдо 1' },
+	{ id: 'food', name: 'Блюдо 2' },
 	{ id: 'salad', name: 'Салаты' },
-	{ id: 'food', name: 'Овқат' },
 	{ id: 'drink', name: 'Ичимлик' },
 	{ id: 'shasha', name: 'Шашлик' },
 	{ id: 'disertlar', name: 'Дисертлар' },
