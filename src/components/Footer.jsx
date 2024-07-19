b37bd160fc52e1d5eeb0adb59cd2e06075dfb066
Shohruz <shokhruzisroilov@gmail.com>
2024-07-19 19:48:49 +0500

    add adress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,20 @@ const Footer = () => {
 		<footer className='bg-white text-black py-8'>
 			<div className='container mx-auto text-center'>
 				<h1 className='text-2xl font-bold mb-4 font-merriweather'>
-					Баҳор ресторанни
+					Bahor restoranni
 				</h1>
 
-				<p className='text-[14px]'>
-					Манзил: 123 Ресторан Кўчаси, Карши, Ўзбекистон
+				<p className='text-[14px] mb-2'>
+					Манзил: Kаманди махалла бахор чойхона, Karshi
 				</p>
-				<p className='text-[14px]'>Телефон: +998 (90) 123-45-67</p>
+				<p className='text-[14px] flex gap-5 items-center justify-center'>
+					Телефонлар:{' '}
+					<ul>
+						<li>+998 (33) 961-08-88</li>
+						<li>+998 (33) 961-08-88</li>
+						<li>+998 (33) 961-08-88</li>
+					</ul>
+				</p>
 			</div>
 		</footer>
 	)
